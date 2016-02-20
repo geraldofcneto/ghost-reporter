98332da084bc07cc892b59ddc925ae6c8b68aa1e
Geraldo Castro <geraldofcneto@gmail.com>
2016-02-20 18:03:49 -0200

    Rendering templates with handlebars

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
@@ -21,8 +19,8 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 // view engine setup
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
+    defaultLayout: 'main',
+    partialsDir: ['views/partials/']
 }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
@@ -31,7 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,7 +39,7 @@ app.use('/users', users);
 app.use('/print', print);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -53,7 +51,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -65,7 +63,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/print.js
+++ routes/print.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var phantom = require('phantom');
 var Report = require('../models/').Report;
+var handlebars = require('handlebars');
 
-router.get('/', create);
-router.get('/google', printGoogle);
+router.post('/', create);
+router.get('/report/:reportId', printReport);
 
 module.exports = router;
 
@@ -15,36 +16,26 @@ function create(req, res) {
   var report = Report.create({ template: template, data: JSON.stringify(data) });
 
   report.then(function (report) {
-  phantom.create(function (ph) {
-    ph.createPage(function (page) {
-      page.open('', function (status) {
-        console.log('JS Evaluated');
-
-        page.includeJs('http://localhost:'+ (process.env.PORT || 3000) + '/js/' /* + templateId */);
-
-        page.set('paperSize', { format: 'A4' });
-
-        page.render('file.pdf', function () {
-          console.log('Printed');
-          res.download('file.pdf', 'file.pdf');
+    phantom.create(function (ph) {
+      ph.createPage(function (page) {
+        page.open('http://localhost:' + (process.env.PORT || 3000) + '/print/report/' + report.dataValues.id, function (status) {
+          page.set('paperSize', { format: 'A4' });
+          var reportFileName = 'report' + report.dataValues.id + '.pdf';
+          page.render(reportFileName, function () {
+            console.log('Printed');
+            res.download('report' + report.dataValues.id + '.pdf', 'report' + report.dataValues.id + '.pdf');
+          });
         });
       });
     });
   });
 }
 
-function printGoogle(req, res) {
-
-  phantom.create(function (ph) {
-    ph.createPage(function (page) {
-      page.set('paperSize', { format: 'A4' });
-      page.open("http://www.google.com", function (status) {
-        page.render('file.pdf', function () {
-          console.log('Printed');
-        });
-      });
-    });
+function printReport(req, res) {
+  var reportId = req.params.reportId;
+  Report.findById(reportId).then(function (report) {
+    var template = handlebars.compile(report.template);
+    var html = template(JSON.parse(report.data));
+    res.send(html);
   });
-
-  res.download('file.pdf', 'file:///file.pdf');
 }
